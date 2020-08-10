98e97d9645241950d4403c2c3a9dcc288ca8ee88
ubuntu-yumaa <yumaaa312@gmail.com>
2020-08-11 04:15:33 +0900

    change script for dev

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,13 @@
 module.exports = {
-  publicPath: '/vuetify_practice',
-  outputDir: 'docs',
-  "transpileDependencies": [
-    "vuetify"
-  ]
+
+    // build先ディレクトリ
+    outputDir: 'docs',
+    // 静的ファイル公開設定
+    publicPath: process.env.NODE_ENV === 'production'
+        ? '/vuetify-training'
+        : '/'
+    ,
+    "transpileDependencies": [
+        "vuetify"
+    ]
 }
