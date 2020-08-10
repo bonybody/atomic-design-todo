d30b7e291f26d201d6b847d8f20fd87e8183d1ad
ubuntu-yumaa <yumaaa312@gmail.com>
2020-08-11 04:20:19 +0900

    project name fix

--- vue.config.js
+++ vue.config.js
@@ -4,7 +4,7 @@ module.exports = {
     outputDir: 'docs',
     // 静的ファイル公開設定
     publicPath: process.env.NODE_ENV === 'production'
-        ? '/vuetify-practice'
+        ? '/vuetify-practice/'
         : '/'
     ,
     "transpileDependencies": [
